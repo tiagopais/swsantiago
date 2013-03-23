0d2b67900180d8343c8f15064381cf5370cd942e
Vijay Khemlani <vkhemlan@gmail.com>
2013-03-23 13:26:32 -0300

    * Added first models

--- commons/models.py
+++ commons/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Auction(models.Model):
+    user = models.ForeignKey(User)
+    creation_date = models.DateField(auto_now_add=True)
+    address = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    auction_type = models.CharField(
+        max_length=255,
+        choices=(
+            ('A', 'Apartment'),
+            ('H', 'House'),
+            ('R', 'Room'))
+    )
+    minimum_bid = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(User)
+    auction = models.ForeignKey(Auction)
+    creation_date = models.DateField(auto_now_add=True)
+    comment = models.TextField()
+    value = models.DecimalField(max_digits=7, decimal_places=2)
